9dc4d42a87fbaef17b5bb517b9e227b54522241b
Apipstexu <aphuy724@gmail.com>
2026-09-08 16:43:38 +0700

    Add files via upload

---
+++ for_loop_dasar.py
@@ -0,0 +1,12 @@
+# Sintaks for loop
+#for variable in urutan:
+    # blok kode yang diulang
+
+# contoh 1:Cetak angka 1 sampai 5
+for i in range(1,6):
+    print("angka:", i)
+
+# Contoh 2: Iterasi karakter dalam string
+for huruf in "PYTHON CODING ":
+    print(huruf, end=" ")
+# Output: P Y T H O N C O D I N G
